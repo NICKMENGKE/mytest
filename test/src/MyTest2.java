046a6a8b3d15a1dd1544919756319230355087d9
“孟可” <mengke@bosssoft.com.cn>
2021-04-28 20:15:42 +0800

    IDEA本地提交PUSH时选择合并看是否覆盖

--- test/src/MyTest2.java
+++ test/src/MyTest2.java
@@ -5,5 +5,6 @@ public class MyTest2 {
     public static void main(String [] ars){
         System.out.println("master 提交测试类");
         System.out.println("main 测试类2修改");
+        System.out.println("main 测试类2修改33333");
     }
 }
